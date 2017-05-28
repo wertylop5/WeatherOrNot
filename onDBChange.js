dbfcc88197875885c734db789add412644a26950
Brian Yang <brian.yang328@gmail.com>
2017-05-28 01:34:31 -0400

    Show danger level

--- onDBChange.js
+++ onDBChange.js
@@ -1,33 +1,53 @@
 const config = {
-	apiKey: "AIzaSyBMitEISRAJbKP6lEjhAYyAsemK_2yhSLE",
-	authDomain: "weatherornot-6545c.firebaseapp.com",
-	databaseURL: "https://weatherornot-6545c.firebaseio.com",
-	projectId: "weatherornot-6545c",
-	storageBucket: "weatherornot-6545c.appspot.com",
-	messagingSenderId: "881439506504"
-  };
+    apiKey: "AIzaSyACgEdPWnk8lOheaJIdTnH2JQ-4W7p9lZY",
+    authDomain: "weatherornot-7c7eb.firebaseapp.com",
+    databaseURL: "https://weatherornot-7c7eb.firebaseio.com",
+    projectId: "weatherornot-7c7eb",
+    storageBucket: "weatherornot-7c7eb.appspot.com",
+    messagingSenderId: "834723964229"
+};
 firebase.initializeApp(config);
 
-const preObject = document.getElementById('test');
 const dbObject = firebase.database().ref();
 
-dbObject.on('value', function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-	var childKey = childSnapshot.key;
-	var childData = childSnapshot.val();
-	console.log(childKey + ", " + childData);
+var getDangerZones = function(lat, lng) {
+    var avg = 0.0;
+    var counter = 0;
+    dbObject.on('value', function(snapshot) {
+	snapshot.forEach(function(childSnapshot) {
+	    // var childKey = childSnapshot.key;
+	    var childData = childSnapshot.val();
+	    var latitude = childData['latit'];
+	    var longitude = childData['longit'];
+	    var comment = childData['comment'];
+	    var dangerlevel = childData['dangerlevel'];
+
+	    console.log(Math.round(parseFloat(lat) * 100));
+	    console.log(Math.round(parseFloat(latitude) * 100));
+	    if (Math.abs(Math.round(parseFloat(lat) * 100) - Math.round(parseFloat(latitude) * 100)) < 2 && Math.abs(Math.round(parseFloat(lng) * 100) - Math.round(parseFloat(longitude) * 100)) < 2) {
+		avg = avg + parseInt(dangerlevel);
+		counter++;
+	    }
+	    // console.log(comment);
+	});
     });
-});
+    if (counter === 0) {
+	return -1;
+    } else {
+	return avg / counter;
+    }
+}
 
-
-// Work in progress
-function writeNewPost(latitude, longitude) {
+function writeNewPost(latitude, longitude, dangerlevel, comment) {
     console.log(latitude);
     console.log(longitude);
+    console.log(comment);
     // A post entry.
     var postData = {
 	latit: latitude,
-	longit: longitude
+	longit: longitude,
+	dangerlevel: dangerlevel,
+	comment: comment
     };
 
     // Get a key for a new Post.
